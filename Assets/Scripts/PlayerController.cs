66e3c3ca778a0e2eeea08f6c5ca16d2658651f43
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-04-30 01:20:42 +0300

    trying to figuer out how to rotate the character to the direction of the camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,9 @@ public class PlayerController : MonoBehaviour
     Camera followCamera;
     Rigidbody rb;
 
+
     float speed = 100.0f;
-    float rotationSpeed = 5.0f;
+    float rotationSpeed = 10.0f;
 
     [SerializeField]
     InputSC inputValues;
@@ -22,12 +23,9 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        //if (inputValues.playerMovement != Vector2.zero)
-        //{
-        //    float targetAngle = Mathf.Atan2(inputValues.mousePosition.x, inputValues.mousePosition.y) * Mathf.Rad2Deg * followCamera.transform.eulerAngles.y;
-        //    Quaternion targetRotation = Quaternion.Euler(0.0f, targetAngle, 0.0f);
-        //    transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
-        //}
+        AlignCameraWithPlayer();
+        Debug.DrawLine(transform.position, transform.forward * 100.0f, Color.red);
+        Debug.DrawLine(followCamera.transform.position, followCamera.transform.forward * 100.0f, Color.blue);
     }
 
     // Update is called once per frame
@@ -37,11 +35,33 @@ public class PlayerController : MonoBehaviour
     }
 
 
+    /// <summary>
+    /// Move the Rigid body of the player
+    /// </summary>
     void Walk()
     {
         Debug.Log(inputValues.playerMovement);
         Vector3 movementDirection = new Vector3(inputValues.playerMovement.x, rb.velocity.y, inputValues.playerMovement.y);
-        //movementDirection = transform.InverseTransformDirection(movementDirection);
+
+        // apply the rigid body velocity to the local transform
         rb.velocity = transform.TransformDirection(movementDirection * speed * Time.deltaTime);
+    }
+
+
+    /// <summary>
+    /// Align the camera with player while the player is moving
+    /// </summary>
+    void AlignCameraWithPlayer()
+    {
+        if (inputValues.playerMovement != Vector2.zero)
+        {
+            //float targetAngle = Mathf.Atan2(inputValues.mousePosition.x, inputValues.mousePosition.y) * Mathf.Rad2Deg * followCamera.transform.eulerAngles.y;
+            //Quaternion targetRotation = Quaternion.Euler(0.0f, targetAngle, 0.0f);
+            //transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+
+            Quaternion targetRotation = Quaternion.FromToRotation(transform.forward, followCamera.transform.forward);
+            targetRotation.y = 0.0f;
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation.normalized, Time.deltaTime * rotationSpeed);
+        }
     }
 }
